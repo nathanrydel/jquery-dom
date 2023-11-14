ca660c2a97ed73519e34f2f44c5372e0b89b1e72
Nathan Rydel <nathanrydel@gmail.com>
2023-11-14 13:51:50 -0600

    Updated the code for each movie entry and fixed delete button use

--- Part 2/scripts.js
+++ Part 2/scripts.js
@@ -1,22 +1,23 @@
 "use strict";
 
-const $deleteBtn = $('<button class="delete-btn">Delete</button>');
+const $button = $('<button class="delete-btn">Delete</button>');
 
 $("form").on("submit", function (evt) {
   const $formInput = $("input");
   const $movieTitle = $formInput.eq(0).val();
   const $movieRating = $formInput.eq(1).val();
 
-  // TODO: Refactor to separate ratings from titles
-  const movie = $("<div>", { text: `${$movieTitle} ${$movieRating}` }).addClass('movie');
-  $("#movie-container").append(movie);
-  $('.movie').append($deleteBtn);
+  const $movie = $("<div>").addClass('movie');
+  $movie.append("<span>" + $movieTitle + "</span>");
+  $movie.append("<span>" + $movieRating + "</span>");
+  $movie.append($button.clone());
+  $("#movie-container").append($movie);
   evt.preventDefault();
 });
 
-$('#movie-container').on('click', '.movie', removeButton);
+$('#movie-container').on('click', '.delete-btn', deleteMovieEntry);
 
 /** Ignores what we did above and just finds a way to remove the right div */
-function removeButton(evt) {
-  $(evt.target).parent().remove(); //FIXME: i hate this
+function deleteMovieEntry(evt) {
+  $(evt.target).closest(".movie").remove();
 }
