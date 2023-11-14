f228881ba9128218c2b078b1c99c7dec502871ba
Nathan Rydel <nathanrydel@gmail.com>
2023-11-13 18:47:30 -0600

    Part Two TODOs

---
+++ Part 2/scripts.js
@@ -0,0 +1,8 @@
+// TODO: Handle form submission
+// Capture the values for each input
+// add them to the #movie-container
+
+//TODO:
+// add a button to each movie entry that removes it from the DOM
+
+// TODO: Handle button click that removes movie entry
