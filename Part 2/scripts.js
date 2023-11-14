bde84a47b68845e7348c493cc7c8dace23ecb658
Nathan Rydel <nathanrydel@gmail.com>
2023-11-13 19:08:24 -0600

    First req done

--- Part 2/scripts.js
+++ Part 2/scripts.js
@@ -1,7 +1,16 @@
+"use strict";
 // TODO: Handle form submission
 // Capture the values for each input
 // add them to the #movie-container
 
+$("form").on("submit", function (evt) {
+  const $formInput = $("input");
+  const $movieTitle = $formInput.eq(0).val();
+  const $movieRating = $formInput.eq(1).val();
+
+  $("#movie-container").append("<div>").append(`Title: ${$movieTitle}, Rating: ${$movieRating}`);
+  evt.preventDefault();
+});
 //TODO:
 // add a button to each movie entry that removes it from the DOM
 
