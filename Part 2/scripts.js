dcddcfac8559a93941872a48a5df494897b5b832
Nathan Rydel <nathanrydel@gmail.com>
2023-11-14 13:29:06 -0600

    Minor tweaks

--- Part 2/scripts.js
+++ Part 2/scripts.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const $button = $('<button class="remove">test</button>');
+const $button = $('<button class="delete-btn">Delete</button>');
 
 $("form").on("submit", function (evt) {
   const $formInput = $("input");
@@ -8,17 +8,16 @@ $("form").on("submit", function (evt) {
   const $movieRating = $formInput.eq(1).val();
 
   //$("#movie-container").append("<div class='movie'>").append(`Title: ${movieTitle}, Rating: ${movieRating}`);
-  const movie = $("<div>",  {text:`${$movieTitle} ${$movieRating}`}).addClass('movie');
+  const movie = $("<div>", { text: `${$movieTitle} ${$movieRating}` }).addClass('movie');
   $("#movie-container").append(movie);
   //const $button = $('<button class="remove">test</button>');
   $('.movie').append($button);
-  console.log($button);
   evt.preventDefault();
 });
 
-$('#movie-container').on('click','.movie',removeButton);
+$('#movie-container').on('click', '.movie', removeButton);
 
 /** Ignores what we did above and just finds a way to remove the right div */
-function removeButton(evt){
+function removeButton(evt) {
   $(evt.target).parent().remove(); //FIXME: i hate this
 }
