a7ba2773340dc8a67fea152d36ee015dff7b7248
Nathan Rydel <nathanrydel@gmail.com>
2023-12-28 10:27:41 -0700

    updated jquery into separate file

---
+++ scripts.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// Give all images inside of an article tag the class of image-center (this class is defined inside of the style tag in the head).
+$("article img").addClass("image-center");
+
+// Remove the last paragraph in the article.
+$("article p:last-child").remove();
+
+// Set the font size of the title to be a random pixel size from 0 to 100.
+$("title").css("font-size", Math.random() * 100);
+
+// Add an item to the list; it can say whatever you want.
+$("ol").append("<li>", { text: "Here's an unordered list element" });
+
+// Scratch that; the list is silly. Empty the aside and put a paragraph in it apologizing for the list’s existence.
+$("aside")
+    .empty()
+    .append("<p>", { text: "Sorry for this list's existence!" });
+
+// When you change the numbers in the three inputs on the bottom, the background color of the body should change to match whatever the three values in the inputs are. HINT - The change event is triggered when the value of an input changes.
+$(".form-control").on("change", function () {
+    let red = $(".form-control").eq(0).val();
+    let blue = $(".form-control").eq(1).val();
+    let green = $(".form-control").eq(2).val();
+
+    $("body").css("background-color", `rgb(${red}, ${green}, ${blue})`);
+});
+
+
+// Add an event listener so that when you click on the image, it is removed from the DOM.
+
+$("img").on("click", function (evt) {
+    $(evt.target).remove();
+});
